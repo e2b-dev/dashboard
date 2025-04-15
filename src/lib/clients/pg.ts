b4da29f45a64eae0d37691c00a61ea8162e709b5
Ben Fornefeld <ben.fornefeld@gmail.com>
2025-04-15 15:15:43 +0200

    update: allow self-signed ssl certification for pg client

--- src/lib/clients/pg.ts
+++ src/lib/clients/pg.ts
@@ -6,7 +6,9 @@ const sql = postgres(process.env.DATABASE_URL, {
   // We use supabase transaction pooler url to connect to the database
   // Hence we disable prepared statements to avoid compatibility issues
   prepare: false,
-  ssl: true,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 })
 
 export default sql
