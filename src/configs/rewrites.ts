19d9288f4c69a19075716ef568e02e7a03c48d1d
Ben Fornefeld <ben.fornefeld@gmail.com>
2025-04-24 20:32:25 +0200

    remove: framer domain config & sitemap

--- src/configs/rewrites.ts
+++ src/configs/rewrites.ts
@@ -1,7 +1,6 @@
 import { DomainConfig } from '@/types/rewrites.types'
 
 export const LANDING_PAGE_DOMAIN = 'www.e2b-landing-page.com'
-export const LANDING_PAGE_FRAMER_DOMAIN = 'e2b-landing-page.framer.website'
 export const DOCS_NEXT_DOMAIN = 'e2b-docs.vercel.app'
 
 // Currently we have two locations for rewrites to happen.
