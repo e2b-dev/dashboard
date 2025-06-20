626810c6d355b725d0c6965dd29667a582683cad
Ben Fornefeld <ben.fornefeld@gmail.com>
2025-06-20 20:24:35 +0200

    improve: inspect fetch caching and streaming structure

--- src/features/dashboard/sandboxes/table-row.tsx
+++ src/features/dashboard/sandboxes/table-row.tsx
@@ -3,15 +3,15 @@ import { Row } from '@tanstack/react-table'
 import { DataTableCell, DataTableRow } from '@/ui/data-table'
 import { flexRender } from '@tanstack/react-table'
 import { SandboxWithMetrics } from './table-config'
-import { useParams, useRouter, useSelectedLayoutSegment } from 'next/navigation'
+import { useParams } from 'next/navigation'
 import { PROTECTED_URLS } from '@/configs/urls'
+import Link from 'next/link'
 
 interface TableRowProps {
   row: Row<SandboxWithMetrics>
 }
 
 export const TableRow = memo(function TableRow({ row }: TableRowProps) {
-  const router = useRouter()
   const { teamIdOrSlug } = useParams()
 
   if (!teamIdOrSlug || typeof teamIdOrSlug !== 'string') {
@@ -19,24 +19,23 @@ export const TableRow = memo(function TableRow({ row }: TableRowProps) {
   }
 
   return (
-    <DataTableRow
-      key={row.id}
-      isSelected={row.getIsSelected()}
-      className="h-8 cursor-pointer border-b"
-      onClick={() => {
-        router.push(
-          PROTECTED_URLS.SANDBOX_INSPECT(
-            teamIdOrSlug,
-            `${row.original.sandboxID}-${row.original.clientID}`
-          )
-        )
-      }}
+    <Link
+      href={PROTECTED_URLS.SANDBOX_INSPECT(
+        teamIdOrSlug,
+        row.original.sandboxID
+      )}
     >
-      {row.getVisibleCells().map((cell) => (
-        <DataTableCell key={cell.id} cell={cell}>
-          {flexRender(cell.column.columnDef.cell, cell.getContext())}
-        </DataTableCell>
-      ))}
-    </DataTableRow>
+      <DataTableRow
+        key={row.id}
+        className="hover:bg-bg-100 h-8 cursor-pointer border-b"
+        isSelected={row.getIsSelected()}
+      >
+        {row.getVisibleCells().map((cell) => (
+          <DataTableCell key={cell.id} cell={cell}>
+            {flexRender(cell.column.columnDef.cell, cell.getContext())}
+          </DataTableCell>
+        ))}
+      </DataTableRow>
+    </Link>
   )
 })
