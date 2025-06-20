130a737f2a1f6c24840e7c2a3467680351ccee84
Ben Fornefeld <ben.fornefeld@gmail.com>
2025-06-20 20:24:35 +0200

    improve: inspect fetch caching and streaming structure

--- src/features/dashboard/sandboxes/table-row.tsx
+++ src/features/dashboard/sandboxes/table-row.tsx
@@ -5,38 +5,33 @@ import { flexRender } from '@tanstack/react-table'
 import { Sandbox } from '@/types/api'
 import { useParams, useRouter, useSelectedLayoutSegment } from 'next/navigation'
 import { PROTECTED_URLS } from '@/configs/urls'
+import Link from 'next/link'
 
 interface TableRowProps {
   row: Row<Sandbox>
 }
 
 export const TableRow = memo(function TableRow({ row }: TableRowProps) {
-  const router = useRouter()
   const { teamIdOrSlug } = useParams()
+  const sandboxId = `${row.original.sandboxID}-${row.original.clientID}`
 
   if (!teamIdOrSlug || typeof teamIdOrSlug !== 'string') {
     return null
   }
 
   return (
-    <DataTableRow
-      key={row.id}
-      onClick={() => {
-        router.push(
-          PROTECTED_URLS.SANDBOX_INSPECT(
-            teamIdOrSlug,
-            `${row.original.sandboxID}-${row.original.clientID}`
-          )
-        )
-      }}
-      className="cursor-pointer"
-      isSelected={row.getIsSelected()}
-    >
-      {row.getVisibleCells().map((cell) => (
-        <DataTableCell key={cell.id} cell={cell}>
-          {flexRender(cell.column.columnDef.cell, cell.getContext())}
-        </DataTableCell>
-      ))}
-    </DataTableRow>
+    <Link href={PROTECTED_URLS.SANDBOX_INSPECT(teamIdOrSlug, sandboxId)}>
+      <DataTableRow
+        key={row.id}
+        className="hover:bg-bg-100 cursor-pointer"
+        isSelected={row.getIsSelected()}
+      >
+        {row.getVisibleCells().map((cell) => (
+          <DataTableCell key={cell.id} cell={cell}>
+            {flexRender(cell.column.columnDef.cell, cell.getContext())}
+          </DataTableCell>
+        ))}
+      </DataTableRow>
+    </Link>
   )
 })
