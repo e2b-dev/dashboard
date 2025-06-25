be5df3b7ffca0e48a6e122c5e7f23edb2f1cd391
Ben Fornefeld <ben.fornefeld@gmail.com>
2025-06-25 17:40:14 +0200

    feat: directory loading & error states

--- src/features/dashboard/sandbox/inspect/dir.tsx
+++ src/features/dashboard/sandbox/inspect/dir.tsx
@@ -1,9 +1,9 @@
 import { FilesystemNode } from './filesystem/types'
+import type { CSSProperties } from 'react'
 import { ChevronRight } from 'lucide-react'
 import SandboxInspectNode from './node'
 import { useDirectory } from './hooks/use-directory'
 import { cn } from '@/lib/utils'
-import { Loader } from '@/ui/loader'
 import { DataTableRow } from '@/ui/data-table'
 import { motion } from 'motion/react'
 import { FileType } from 'e2b'
@@ -15,40 +15,59 @@ interface SandboxInspectDirProps {
 }
 
 export default function SandboxInspectDir({ dir }: SandboxInspectDirProps) {
-  const { isExpanded, toggle, isLoading, hasChildren, children } = useDirectory(
-    dir.path
-  )
+  const {
+    hasError,
+    error,
+    isExpanded,
+    toggle,
+    isLoading,
+    isLoaded,
+    hasChildren,
+    children,
+  } = useDirectory(dir.path)
 
   return (
     <>
       <DataTableRow
         role="button"
         tabIndex={0}
-        onClick={() => toggle()}
-        onKeyDown={(e) => {
-          if (e.key === 'Enter' || e.key === ' ') {
-            toggle()
-          }
-        }}
+        onClick={toggle}
         className={cn(
-          'hover:bg-bg-200 cursor-pointer gap-1 py-1 transition-none select-none',
-          {
-            'animate-pulse': isLoading,
-          }
+          'hover:bg-bg-200 cursor-pointer gap-1 truncate py-1 transition-none select-none'
         )}
       >
         <motion.span
           animate={{
-            rotate: isExpanded ? 90 : 0,
+            rotate: isExpanded && isLoaded ? 90 : 0,
+            color:
+              isExpanded && isLoaded
+                ? 'var(--color-fg)'
+                : 'var(--color-fg-500)',
           }}
         >
-          <ChevronRight className="text-fg-300 size-4" />
+          <ChevronRight className="size-4" />
         </motion.span>
-        <span className="text-left font-sans">{dir.name}</span>
-        {isLoading && <Loader className="ml-2" />}
+        <span
+          style={{ '--shiny-width': '100px' } as CSSProperties}
+          className={cn('truncate text-left font-sans', {
+            // Shine effect
+            'text-fg-500 animate-shiny-text [background-size:var(--shiny-width)_100%] bg-clip-text [background-position:0_0] bg-no-repeat duration-1000 [transition:background-position_1s_ease-in-out]':
+              isLoading,
+            // Shine gradient
+            'bg-gradient-to-r from-transparent via-[var(--color-fg)] via-50% to-transparent':
+              isLoading,
+          })}
+        >
+          {dir.name}
+        </span>
+        {hasError && (
+          <span className="text-error truncate text-sm text-ellipsis">
+            {error}
+          </span>
+        )}
       </DataTableRow>
 
-      {isExpanded && (
+      {isExpanded && hasChildren && (
         <div className="flex flex-col pl-2">
           {children.map((child) => (
             <SandboxInspectNode key={child.path} path={child.path} />
