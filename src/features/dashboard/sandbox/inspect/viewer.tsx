10db389371de9af54b28ec8a9bbbcd5294433e43
Ben Fornefeld <ben.fornefeld@gmail.com>
2025-06-30 17:12:42 +0200

    refactor: remove access of node state through node

--- src/features/dashboard/sandbox/inspect/viewer.tsx
+++ src/features/dashboard/sandbox/inspect/viewer.tsx
@@ -3,12 +3,11 @@
 import { useContent } from './hooks/use-content'
 import { useShikiTheme } from '@/configs/shiki'
 import ShikiHighlighter, { Language } from 'react-shiki'
-import { useFilesystemNode, useSelectedPath } from './hooks/use-node'
-import { FileType } from 'e2b'
+import { useSelectedPath } from './hooks/use-node'
 import SandboxInspectFrame from './frame'
 import SandboxInspectViewerHeader from './viewer-header'
 import { ScrollArea, ScrollBar } from '@/ui/primitives/scroll-area'
-import { useFileOperations } from './hooks/use-file'
+import { useFile } from './hooks/use-file'
 
 export default function SandboxInspectViewer() {
   const path = useSelectedPath()
@@ -21,19 +20,18 @@ export default function SandboxInspectViewer() {
 }
 
 function SandboxInspectViewerContent({ path }: { path: string }) {
-  const node = useFilesystemNode(path)
-  const { refresh } = useFileOperations(path)
+  const { name, isLoading, refresh } = useFile(path)
   const { content } = useContent(path)
   const shikiTheme = useShikiTheme()
 
-  if (content === undefined || node?.type !== FileType.FILE) {
+  if (content === undefined) {
     return null
   }
 
-  const hasDot = node.name.includes('.')
-  let language: Language = node.name.split('.').pop() ?? 'text'
+  const hasDot = name.includes('.')
+  let language: Language = name.split('.').pop() ?? 'text'
 
-  if (!hasDot || (node.name.startsWith('.') && language)) {
+  if (!hasDot || (name.startsWith('.') && language)) {
     language = 'text'
   }
 
@@ -42,9 +40,9 @@ function SandboxInspectViewerContent({ path }: { path: string }) {
       className="max-w-1/2"
       header={
         <SandboxInspectViewerHeader
-          name={node.name}
+          name={name}
           content={content}
-          isLoading={node.isLoading ?? false}
+          isLoading={isLoading}
           onRefresh={refresh}
         />
       }
