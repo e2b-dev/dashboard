1e891c65aa9ebf49976960a42a1de98fea4141fc
Ben Fornefeld <ben.fornefeld@gmail.com>
2025-07-01 12:48:35 +0200

    feat: file viewer responsiveness

--- src/features/dashboard/sandbox/inspect/viewer.tsx
+++ src/features/dashboard/sandbox/inspect/viewer.tsx
@@ -3,17 +3,40 @@
 import { useContent } from './hooks/use-content'
 import { useShikiTheme } from '@/configs/shiki'
 import ShikiHighlighter, { Language } from 'react-shiki'
-import { useSelectedPath } from './hooks/use-node'
+import { useErrorPaths, useSelectedPath } from './hooks/use-node'
 import SandboxInspectFrame from './frame'
 import SandboxInspectViewerHeader from './viewer-header'
 import { ScrollArea, ScrollBar } from '@/ui/primitives/scroll-area'
 import { useFile } from './hooks/use-file'
+import { Drawer, DrawerContent } from '@/ui/primitives/drawer'
+import { useIsMobile } from '@/lib/hooks/use-mobile'
+import { useEffect, useState } from 'react'
 
 export default function SandboxInspectViewer() {
   const path = useSelectedPath()
+  const isMobile = useIsMobile()
+  const errorPaths = useErrorPaths()
+
+  const [open, setOpen] = useState(false)
+
+  useEffect(() => {
+    if (path && !errorPaths.has(path)) {
+      setOpen(true)
+    }
+  }, [path, errorPaths])
 
   if (!path) {
     return null
+  }
+
+  if (isMobile) {
+    return (
+      <Drawer open={open} onOpenChange={setOpen}>
+        <DrawerContent className="mt-4 h-[90svh] overflow-hidden p-0">
+          <SandboxInspectViewerContent path={path} />
+        </DrawerContent>
+      </Drawer>
+    )
   }
 
   return <SandboxInspectViewerContent path={path} />
@@ -37,7 +60,10 @@ function SandboxInspectViewerContent({ path }: { path: string }) {
 
   return (
     <SandboxInspectFrame
-      className="max-w-1/2"
+      classNames={{
+        frame: 'max-w-1/2 max-md:max-w-full max-md:border-none',
+        header: 'max-md:bg-transparent max-md:h-9',
+      }}
       header={
         <SandboxInspectViewerHeader
           name={name}
@@ -52,7 +78,7 @@ function SandboxInspectViewerContent({ path }: { path: string }) {
           <ShikiHighlighter
             language={language}
             theme={shikiTheme}
-            className="px-1.5 py-1 text-sm"
+            className="px-1.5 py-1 text-sm max-md:p-3"
             addDefaultStyles={false}
             showLanguage={false}
           >
