d6c58685dffbf717c550b0c7a4994bf55603c08c
ben-fornefeld <ben.fornefeld@gmail.com>
2025-07-29 17:30:14 +0200

    refactor: optimize TemplateId component by using useMemo for value calculation and improving readability; replace direct sandboxInfo access with memoized value

--- src/features/dashboard/sandbox/header/template-id.tsx
+++ src/features/dashboard/sandbox/header/template-id.tsx
@@ -3,18 +3,25 @@
 import CopyButton from '@/ui/copy-button'
 import { Badge } from '@/ui/primitives/badge'
 import { useSandboxContext } from '../context'
+import { useMemo } from 'react'
 
 export default function TemplateId() {
   const { sandboxInfo } = useSandboxContext()
 
+  console.log('sandboxInfo', sandboxInfo)
+
+  const value = useMemo(() => {
+    return sandboxInfo?.alias || sandboxInfo?.templateID?.toString() || ''
+  }, [sandboxInfo])
+
   return (
     <Badge variant="contrast-2" className="gap-2.5">
-      <p>{sandboxInfo?.alias ?? sandboxInfo?.templateID?.toString()}</p>
+      <p>{value}</p>
       <CopyButton
         size="slate"
         className="size-3.5"
         variant="ghost"
-        value={sandboxInfo?.alias ?? sandboxInfo?.templateID?.toString() ?? ''}
+        value={value}
       />
     </Badge>
   )
