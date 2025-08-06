5ad4bd181f147ad1d2c39cd52f86248ac7ef2dbf
ben-fornefeld <ben.fornefeld@gmail.com>
2025-08-06 14:55:49 +0200

    refactor: update input component hover and focus styles

--- src/ui/primitives/input.tsx
+++ src/ui/primitives/input.tsx
@@ -17,8 +17,8 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
           'bg-bg flex h-8 w-full border px-3 py-2',
           '!text-body placeholder:text-fg-tertiary',
 
-          'focus:[border-bottom:1px_solid_var(--accent-main-highlight )] focus:outline-none',
-          'hover:bg-bg-hover focus:bg-bg-highlight',
+          'focus:[border-bottom:1px_solid_var(--accent-main-highlight)] focus:outline-none',
+          'hover:bg-bg-hover focus:bg-bg-hover',
           'disabled:cursor-not-allowed disabled:opacity-50',
 
           'file:border-0 file:bg-transparent',
