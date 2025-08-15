38063dd02b3b00aebf2769ddda967e02c2c59eca
ben-fornefeld <ben.fornefeld@gmail.com>
2025-08-15 15:56:25 +0200

    fix: badge to take in new variants

--- src/ui/primitives/badge.tsx
+++ src/ui/primitives/badge.tsx
@@ -4,7 +4,7 @@ import { cva, type VariantProps } from 'class-variance-authority'
 import * as React from 'react'
 
 const badgeVariants = cva(
-  'inline-flex items-center cursor-default justify-center prose-label-highlight focus-visible:ring-1 w-fit whitespace-nowrap shrink-0 [&>svg]:size-3  [&>svg]:pointer-events-none ![&>svg]:pl-0.75 aria-invalid:ring-accent-error-highlight/20 aria-invalid:border-accent-error-highlight transition-[color,box-shadow] overflow-hidden',
+  'inline-flex items-center cursor-default justify-center prose-label-highlight focus-visible:ring-1 w-fit whitespace-nowrap shrink-0 [&>svg]:size-3 [&>svg]:pointer-events-none ![&>svg]:pl-0.75 aria-invalid:ring-accent-error-highlight/20 aria-invalid:border-accent-error-highlight transition-[color,box-shadow] overflow-hidden',
   {
     variants: {
       variant: {
@@ -15,14 +15,14 @@ const badgeVariants = cva(
         error: 'bg-accent-error-bg text-accent-error-highlight',
         code: 'bg-bg-1 ring-1 ring-stroke text-fg-secondary font-mono',
       },
-      can: {
-        none: '',
-        hover: 'hover:ring-1 ring-[currentColor]',
-      },
       size: {
         default: 'h-5 px-1 gap-1',
         sm: 'h-4.5 px-1 text-xs gap-0.5',
-        lg: 'h-7 px-2.5 gap-1.5',
+        lg: 'h-7 px-2 gap-1.5',
+      },
+      can: {
+        none: '',
+        hover: 'hover:ring-1 ring-[currentColor]',
       },
     },
     defaultVariants: {
@@ -39,13 +39,20 @@ export interface BadgeProps
   asChild?: boolean
 }
 
-function Badge({ className, variant, asChild = false, ...props }: BadgeProps) {
+function Badge({
+  className,
+  variant,
+  size,
+  can,
+  asChild = false,
+  ...props
+}: BadgeProps) {
   const Comp = asChild ? Slot : 'span'
 
   return (
     <Comp
       data-slot="badge"
-      className={cn(badgeVariants({ variant }), className)}
+      className={cn(badgeVariants({ variant, size, can }), className)}
       {...props}
     />
   )
