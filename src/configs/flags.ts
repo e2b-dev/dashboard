89b966e96e4fa7ec090d8676e8d487c52c787625
ben-fornefeld <ben.fornefeld@gmail.com>
2025-09-09 16:26:18 +0200

    chore: remove deprecated chart warning line flag

--- src/configs/flags.ts
+++ src/configs/flags.ts
@@ -2,5 +2,3 @@ export const ALLOW_SEO_INDEXING = process.env.ALLOW_SEO_INDEXING === '1'
 export const VERBOSE = process.env.NEXT_PUBLIC_VERBOSE === '1'
 export const INCLUDE_BILLING = process.env.NEXT_PUBLIC_INCLUDE_BILLING === '1'
 export const USE_MOCK_DATA = process.env.NEXT_PUBLIC_MOCK_DATA === '1'
-export const ENABLE_CONCURRENT_CHART_WARNING_LINE =
-  process.env.NEXT_PUBLIC_ENABLE_CONCURRENT_CHART_WARNING_LINE === '1'
