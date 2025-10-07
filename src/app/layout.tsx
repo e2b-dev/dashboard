22e2464e1d1545d4b059e54be96bb89821a20aaa
Ben Fornefeld <ben.fornefeld@gmail.com>
2025-10-07 18:31:36 +0200

    refactor: metadata setting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import '@/app/fonts'
 import '@/styles/globals.css'
 
-import { ALLOW_SEO_INDEXING } from '@/configs/flags'
-import { METADATA } from '@/configs/metadata'
 import ClientProviders from '@/features/client-providers'
 import { GeneralAnalyticsCollector } from '@/features/general-analytics-collector'
 import { GTMHead } from '@/features/google-tag-manager'
@@ -12,16 +10,6 @@ import { SpeedInsights } from '@vercel/speed-insights/next'
 import Head from 'next/head'
 import { Suspense } from 'react'
 import { Body } from './layout.client'
-
-export const generateMetadata = () => {
-  return {
-    title: METADATA.title,
-    description: METADATA.description,
-    openGraph: METADATA.openGraph,
-    twitter: METADATA.twitter,
-    robots: ALLOW_SEO_INDEXING ? 'index, follow' : 'noindex, nofollow',
-  }
-}
 
 export default function RootLayout({
   children,
