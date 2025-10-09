6f511279a84fddf97e52be5d4fa5ff38caa8a75c
Ben Fornefeld <ben.fornefeld@gmail.com>
2025-10-09 14:13:15 +0200

    fix: escape dot ascii

--- src/server/team/types.ts
+++ src/server/team/types.ts
@@ -22,7 +22,7 @@ export const TeamNameSchema = z
   .trim()
   .min(1, { message: 'Team name cannot be empty' })
   .max(32, { message: 'Team name cannot be longer than 32 characters' })
-  .regex(/^[a-zA-Z0-9]+(?:[ _.-][a-zA-Z0-9]+)*$/, {
+  .regex(/^[a-zA-Z0-9]+(?:[ _.\-][a-zA-Z0-9]+)*$/, {
     message:
       'Names can only contain letters and numbers, separated by spaces, underscores, hyphens, or dots',
   })
