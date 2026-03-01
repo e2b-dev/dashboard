1be5a80d2020046de50dd71152184e4b87c4a777
ben-fornefeld <ben.fornefeld@gmail.com>
2026-02-28 18:15:30 -0800

    chore: add sandbox details prefetching

--- src/app/dashboard/[teamIdOrSlug]/sandboxes/[sandboxId]/layout.tsx
+++ src/app/dashboard/[teamIdOrSlug]/sandboxes/[sandboxId]/layout.tsx
@@ -2,13 +2,29 @@ import { SandboxProvider } from '@/features/dashboard/sandbox/context'
 import SandboxDetailsControls from '@/features/dashboard/sandbox/header/controls'
 import SandboxDetailsHeader from '@/features/dashboard/sandbox/header/header'
 import SandboxLayoutClient from '@/features/dashboard/sandbox/layout'
+import { prefetch, trpc } from '@/trpc/server'
 
 interface SandboxLayoutProps {
   children: React.ReactNode
   params: Promise<{ teamIdOrSlug: string; sandboxId: string }>
 }
 
-export default async function SandboxLayout({ children }: SandboxLayoutProps) {
+export default async function SandboxLayout({
+  children,
+  params,
+}: SandboxLayoutProps) {
+  const { teamIdOrSlug, sandboxId } = await params
+
+  prefetch(
+    trpc.sandbox.details.queryOptions(
+      { teamIdOrSlug, sandboxId },
+      {
+        retry: false,
+        staleTime: Number.POSITIVE_INFINITY,
+      }
+    )
+  )
+
   return (
     <SandboxProvider>
       <SandboxLayoutClient
