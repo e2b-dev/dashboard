3e1649a725b950cb9cc5f6ae132c9cdc57afb264
ben-fornefeld <ben.fornefeld@gmail.com>
2026-04-16 04:07:17 -0700

    test

--- src/configs/api.ts
+++ src/configs/api.ts
@@ -3,6 +3,7 @@ export const ACCESS_TOKEN_PREFIX = 'sk_e2b_'
 export const SUPABASE_TOKEN_HEADER = 'X-Supabase-Token'
 export const SUPABASE_TEAM_HEADER = 'X-Supabase-Team'
 export const ENVD_ACCESS_TOKEN_HEADER = 'X-Access-Token'
+export const ADMIN_TOKEN_HEADER = 'X-Admin-Token'
 
 export const SUPABASE_AUTH_HEADERS = (token: string, teamId?: string) => ({
   [SUPABASE_TOKEN_HEADER]: token,
@@ -10,6 +11,7 @@ export const SUPABASE_AUTH_HEADERS = (token: string, teamId?: string) => ({
 })
 
 export const ADMIN_AUTH_HEADERS = (token: string) => ({
-  'X-Admin-Token': token,
+  [SUPABASE_TOKEN_HEADER]: 'xxx',
+  [ADMIN_TOKEN_HEADER]: token,
 })
 export const CLI_GENERATED_KEY_NAME = 'CLI login/configure'
