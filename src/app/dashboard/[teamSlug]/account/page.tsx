3f31f1c14443a5562dece55725f93a2086e5ac32
Matt Brockman <matt.brockman@e2b.dev>
2026-05-14 17:48:26 -0700

    Remove Stripe Projects account settings stub

--- src/app/dashboard/[teamSlug]/account/page.tsx
+++ src/app/dashboard/[teamSlug]/account/page.tsx
@@ -2,7 +2,6 @@ import { AccessTokenSettings } from '@/features/dashboard/account/access-token-s
 import { EmailSettings } from '@/features/dashboard/account/email-settings'
 import { NameSettings } from '@/features/dashboard/account/name-settings'
 import { PasswordSettingsServer } from '@/features/dashboard/account/password-settings-server'
-import { StripeProjectsSettings } from '@/features/dashboard/account/stripe-projects-settings'
 
 export interface AccountPageSearchParams {
   reauth?: '1'
@@ -20,8 +19,6 @@ export default async function AccountPage({
       <EmailSettings />
 
       <AccessTokenSettings />
-
-      <StripeProjectsSettings />
 
       <PasswordSettingsServer searchParams={searchParams} />
     </div>
