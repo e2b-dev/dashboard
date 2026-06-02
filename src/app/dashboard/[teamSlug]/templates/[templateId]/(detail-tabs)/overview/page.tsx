4d27ea6cec5fcd696c48844bdcc817f182b7511c
Alex Drankou <alex@e2b.dev>
2026-06-02 12:56:17 +0200

    chore(templates): drop duplicate getTemplate prefetch

--- src/app/dashboard/[teamSlug]/templates/[templateId]/(detail-tabs)/overview/page.tsx
+++ src/app/dashboard/[teamSlug]/templates/[templateId]/(detail-tabs)/overview/page.tsx
@@ -1,14 +1,11 @@
 import { Suspense } from 'react'
 import TemplateOverview from '@/features/dashboard/templates/detail/overview'
 import { TemplateOverviewSkeleton } from '@/features/dashboard/templates/detail/overview/skeleton'
-import { prefetch, trpc } from '@/trpc/server'
 
 export default async function TemplateOverviewPage({
   params,
 }: PageProps<'/dashboard/[teamSlug]/templates/[templateId]'>) {
   const { teamSlug, templateId } = await params
-
-  prefetch(trpc.templates.getTemplate.queryOptions({ teamSlug, templateId }))
 
   return (
     <div className="p-6 md:p-10 flex flex-col gap-6 w-full max-w-[600px] mx-auto">
