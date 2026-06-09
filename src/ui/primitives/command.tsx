cbdbb569fcb7e98df6abf09019e83bb71879805f
Lukáš Huvar <lukas.huvar@e2b.dev>
2026-06-09 19:16:50 +0200

    fix: anchor CMDK dialog to top edge (#373)

--- src/ui/primitives/command.tsx
+++ src/ui/primitives/command.tsx
@@ -25,7 +25,10 @@ Command.displayName = CommandPrimitive.displayName
 const CommandDialog = ({ children, ...props }: DialogProps) => {
   return (
     <Dialog {...props}>
-      <DialogContent className="overflow-hidden p-0" hideClose>
+      <DialogContent
+        className="top-[20svh] max-h-[calc(80svh-1rem)] translate-y-0 overflow-hidden p-0"
+        hideClose
+      >
         <DialogTitle className="sr-only">Search through dashboard</DialogTitle>
         <Command className="[&_[cmdk-group-heading]]:text-fg-tertiary [&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:prose-label-highlight [&_[cmdk-group]]:px-2 [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5">
           {children}
