428524c078000c5e2d0f9a38bce70488c4f95629
Lukáš Huvar <lukas.huvar@e2b.dev>
2026-06-11 17:56:26 +0200

    Revert "feat(auth): custom Ory Elements login UI" (#399)

--- src/app/login/components/custom-divider.tsx
+++
@@ -1,7 +0,0 @@
-'use client'
-
-import TextSeparator from '@/ui/text-separator'
-
-export function OryDivider() {
-  return <TextSeparator text="or" />
-}
--- src/app/login/components/custom-form-group.tsx
+++
@@ -1,7 +0,0 @@
-'use client'
-
-import type { PropsWithChildren } from 'react'
-
-export function OryFormGroup({ children }: PropsWithChildren) {
-  return <div className="flex flex-col gap-4">{children}</div>
-}
