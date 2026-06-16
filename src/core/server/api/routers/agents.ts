ff8fbf3bcbe78388a67b3bf5dba8f3da9dc46f95
Matt Brockman <matt.brockman@e2b.dev>
2026-06-15 17:32:57 -0700

    Read agent templates from feature flag

--- src/core/server/api/routers/agents.ts
+++ src/core/server/api/routers/agents.ts
@@ -1,24 +1,11 @@
 import { resolveAgentTemplates } from '@/configs/agents'
 import { FEATURE_FLAGS } from '@/configs/flags'
-import { createAgentsRepository } from '@/core/modules/agents/repository.server'
 import { featureFlags } from '@/core/server/feature-flags/flags.server'
 import { createTRPCRouter } from '@/core/server/trpc/init'
 import { protectedTeamProcedure } from '@/core/server/trpc/procedures'
 
 export const agentsRouter = createTRPCRouter({
   getTemplates: protectedTeamProcedure.query(async ({ ctx }) => {
-    const agentsRepository = createAgentsRepository({
-      accessToken: ctx.session.access_token,
-      teamId: ctx.teamId,
-    })
-    const agents = await agentsRepository.getAgents()
-
-    if (agents.ok && agents.data.length) {
-      return {
-        templates: resolveAgentTemplates(agents.data),
-      }
-    }
-
     const templates = await featureFlags.getJson(FEATURE_FLAGS.agentTemplates, {
       userId: ctx.session.user.id,
       teamId: ctx.teamId,
