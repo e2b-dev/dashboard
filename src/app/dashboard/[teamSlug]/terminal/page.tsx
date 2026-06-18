3353d82aa4ccbd7463f42295dda1cc837a398514
Matt Brockman <matt.brockman@e2b.dev>
2026-06-18 16:48:23 -0700

    Give team terminal wrapper full height

--- src/app/dashboard/[teamSlug]/terminal/page.tsx
+++ src/app/dashboard/[teamSlug]/terminal/page.tsx
@@ -112,7 +112,7 @@ export default async function TeamTerminalPage({
   }
 
   return (
-    <div className="flex min-h-0 flex-1 overflow-hidden p-3 md:p-6">
+    <div className="flex h-full min-h-0 overflow-hidden p-3 md:p-6">
       <DashboardTerminal
         autoStart
         launchTarget={{
