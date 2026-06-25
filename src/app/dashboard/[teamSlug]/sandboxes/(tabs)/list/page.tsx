6e8d38e2ea9c6262712c7b09239d51b0256bc7d3
Lukáš Huvar <lukas@huvar.cz>
2026-06-25 13:19:19 +0200

    feat: redirect /sandboxes/list to /list2 when new list flag is enabled

--- src/app/dashboard/[teamSlug]/sandboxes/(tabs)/list/page.tsx
+++ src/app/dashboard/[teamSlug]/sandboxes/(tabs)/list/page.tsx
@@ -1,5 +1,8 @@
+import { redirect } from 'next/navigation'
 import { Suspense } from 'react'
+import { PROTECTED_URLS } from '@/configs/urls'
 import LoadingLayout from '@/features/dashboard/loading-layout'
+import { isNewSandboxListEnabled } from '@/features/dashboard/sandboxes/list/feature-flag.server'
 import SandboxesTable from '@/features/dashboard/sandboxes/list/table'
 import { HydrateClient, prefetch, trpc } from '@/trpc/server'
 
@@ -7,6 +10,10 @@ export default async function SandboxesListPage({
   params,
 }: PageProps<'/dashboard/[teamSlug]/sandboxes/list'>) {
   const { teamSlug } = await params
+
+  if (await isNewSandboxListEnabled(teamSlug)) {
+    redirect(PROTECTED_URLS.SANDBOXES_LIST2(teamSlug))
+  }
 
   prefetch(
     trpc.sandboxes.getSandboxes.queryOptions({
